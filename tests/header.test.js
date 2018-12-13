05e26f06c7c68d0664eb86daaff51f04afbadb0d
Miguel Bolivar <MBolivar360@gmail.com>
2018-12-13 12:25:11 -0400

    Adding test to handle oauth flow, with generating a keyGrip and taking a user ID to generate a session cookie and put it into page

--- tests/header.test.js
+++ tests/header.test.js
@@ -24,3 +24,29 @@ test('Click login starts oauth flow', async () => {
   const url = await page.url();
   expect(url).toMatch('/accounts\.google\.com/');
 });
+
+test('When signed in, show logout button', async () => {
+  const id = '5c116703ccff14930b67c083';
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id,
+    },
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+  const KeyGrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new KeyGrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+
+    await page.waitFor('ul.right > li > a[href="/auth/logout"]');
+  const text = await page.$eval('ul.right > li > a[href="/auth/logout"]', el => el.innerHTML);
+  expect(text).toEqual('Logout');
+  
+});
+
