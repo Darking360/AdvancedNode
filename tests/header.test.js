e0f51a267a1a060d9b8ffad939d659096e239b79
Miguel Bolivar <MBolivar360@gmail.com>
2018-12-13 10:53:26 -0400

    Starting to add testing with puppeteer and Chromium based for integration testing

---
+++ tests/header.test.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+beforeEach( async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(() => browser.close());
+
+test('We can launch a browser', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
+  browser.close();
+});
