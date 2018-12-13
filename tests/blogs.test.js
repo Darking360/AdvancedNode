6264ab3d27f7d3470b7b6c1c05c4045bb3f3b6a0
Miguel Bolivar <MBolivar360@gmail.com>
2018-12-13 17:54:41 -0400

    Finishing testing on login and logout cases, for logout testing that user cannot create blog posts or get posts

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -29,7 +29,7 @@ describe('When logged in into create blog page', () => {
     expect(buttonTextSubmit).toContain('Next');
   });
 
-  describe('And using invalid input', () => {
+  describe('And using invalid inputs', () => {
     beforeEach(async () => {
       await page.click('form > button[type="submit"]');
       await page.waitFor('div.red-text');
@@ -42,5 +42,58 @@ describe('When logged in into create blog page', () => {
       expect(contentErrorLabel).toEqual('You must provide a value');
     });
   });
+  
+  describe('And using valid inputs', () => {
+    beforeEach(async () => {
+      await page.type('form > div.title > input', 'Test aiuda 1');
+      await page.type('form > div.content > input', 'Lorem ipsun text');
+      await page.click('form > button[type="submit"]');
+    });
 
+    test('Submitting takes use to review screen', async () => {
+      const text = await page.getContentsOf('form > h5');
+      expect('Please confirm your entries');
+    });
+
+    test('Submitting takes use to review screen, and creates blog post', async () => {
+      await page.click('button.green');
+      await page.waitFor('div.card > div > div > span');
+      const title = await page.getContentsOf('div.card > div > div.card-content > span');
+      expect(title).toEqual('Test aiuda 1');
+      const content = await page.getContentsOf('div.card > div > div.card-content > p');
+      expect(content).toEqual('Lorem ipsun text');
+    });
+  });
+});
+
+describe('When not logged in', () => {
+  test('And tries to submit XHR to create a blog post', async () => {
+    const result = await page.evaluate(() => {
+      return fetch('api/blogs', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: 'Titulo',
+          content: 'Lorem ipsum text',
+        }),
+      }).then((res) => res.json());
+    });
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
+
+  test('And tries to get a list of posts', async () => {
+    const result = await page.evaluate(() => {
+      return fetch('api/blogs', {
+        mthod: 'GET',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => res.json());
+    });
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
 });
